c219b56d81ec13f55a1051d669157dcb44f4a7d2
Hoonki Chae <46335714+mildchae@users.noreply.github.com>
2020-08-31 22:40:31 +0900

    add: snap scoll view
    this is example commit for multi line commit message
    this code for snap horizental scroll view

--- temperature/temperature/ContentView.swift
+++ temperature/temperature/ContentView.swift
@@ -8,7 +8,79 @@
 
 import SwiftUI
 
+struct ScrollingHStackModifier: ViewModifier {
+    
+    @State private var scrollOffset: CGFloat
+    @State private var dragOffset: CGFloat
+    
+    var items: Int
+    var itemWidth: CGFloat
+    var itemSpacing: CGFloat
+    
+    init(items: Int, itemWidth: CGFloat, itemSpacing: CGFloat) {
+        self.items = items
+        self.itemWidth = itemWidth
+        self.itemSpacing = itemSpacing
+        
+        // Calculate Total Content Width
+        let contentWidth: CGFloat = CGFloat(items) * itemWidth + CGFloat(items - 1) * itemSpacing
+        let screenWidth = UIScreen.main.bounds.width
+        
+        // Set Initial Offset to first Item
+        let initialOffset = (contentWidth/2.0) - (screenWidth/2.0) + ((screenWidth - itemWidth) / 2.0)
+        
+        self._scrollOffset = State(initialValue: initialOffset)
+        self._dragOffset = State(initialValue: 0)
+    }
+    
+    func body(content: Content) -> some View {
+        content
+            .offset(x: scrollOffset + dragOffset, y: 0)
+            .gesture(DragGesture()
+                .onChanged({ event in
+                    self.dragOffset = event.translation.width
+                })
+                .onEnded({ event in
+                    // Scroll to where user dragged
+                    self.scrollOffset += event.translation.width
+                    self.dragOffset = 0
+                    
+                    // Now calculate which item to snap to
+                    let contentWidth: CGFloat = CGFloat(self.items) * self.itemWidth + CGFloat(self.items - 1) * self.itemSpacing
+                    let screenWidth = UIScreen.main.bounds.width
+                    
+                    // Center position of current offset
+                    let center = self.scrollOffset + (screenWidth / 2.0) + (contentWidth / 2.0)
+                    
+                    // Calculate which item we are closest to using the defined size
+                    var index = (center - (screenWidth / 2.0)) / (self.itemWidth + self.itemSpacing)
+                    
+                    // Should we stay at current index or are we closer to the next item...
+                    if index.remainder(dividingBy: 1) > 0.5 {
+                        index += 1
+                    } else {
+                        index = CGFloat(Int(index))
+                    }
+                    
+                    // Protect from scrolling out of bounds
+                    index = min(index, CGFloat(self.items) - 1)
+                    index = max(index, 0)
+                    
+                    // Set final offset (snapping to item)
+                    let newOffset = index * self.itemWidth + (index - 1) * self.itemSpacing - (contentWidth / 2.0) + (screenWidth / 2.0) - ((screenWidth - self.itemWidth) / 2.0) + self.itemSpacing
+                    
+                    // Animate snapping
+                    withAnimation {
+                        self.scrollOffset = newOffset
+                    }
+                    
+                })
+            )
+    }
+}
+
 struct ContentView: View {
+    var colors: [Color] = [.blue, .green, .orange, .red, .gray, .pink, .yellow]
     var body: some View {
         VStack {
             Title()
@@ -22,7 +94,23 @@ struct ContentView: View {
                 }
                 Spacer()
             }.padding(.all, 20)
-
+//            ScrollView(.horizontal, showsIndicators: true) {
+//                HStack(alignment: .center, spacing: 50) {
+//                    ForEach(colors, id: \.self) { color in
+//                        Rectangle()
+//                            .foregroundColor(color)
+//                            .frame(width: 200, height: 300, alignment: .leading)
+//                    }
+//                }
+//            }
+            HStack(alignment: .center, spacing: 30) {
+                ForEach(0..<self.colors.count) { i in
+                    self.colors[i]
+                         .frame(width: 250, height: 400, alignment: .center)
+                         .cornerRadius(10)
+                    
+                }
+            }.modifier(ScrollingHStackModifier(items: self.colors.count, itemWidth: 250, itemSpacing: 30))
             Spacer()
         }
     }
